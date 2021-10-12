1e795481524930b48541926aa2f08c9ca57fa023
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2021-10-12 16:39:47 +0900

    main
    main

---
+++ src/app/users-groups/users-groups.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-users-groups',
+  templateUrl: './users-groups.component.html',
+  styleUrls: ['./users-groups.component.css']
+})
+export class UsersGroupsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
